d4d35ae053d97c1ed64e61871ada6955866c1ada
Jianfeng Li <jianfeng.li@pwc.com>
2023-08-15 15:52:38 +0930

    Add clarity to buffer demo

--- cmd/buffered/main.go
+++ cmd/buffered/main.go
@@ -4,25 +4,25 @@ import (
 	"math/rand"
 	"time"
 
-	"funmech.com/loadbalancer"
+	lb "funmech.com/loadbalancer"
 )
 
 func workFn() int {
-	time.Sleep(time.Duration(rand.Intn(9000)) * time.Millisecond)
-	// fmt.Println("I did something for the balancer.")
+	time.Sleep(time.Duration(rand.Intn(9)) * time.Second)
 	return 1
 }
 
 // An artificial but illustrative simulation of a requester, a load generator.
 // work is a send-only channel, once set, Balancer can start to dispatch
-func requester(work chan<- loadbalancer.Request, nWorker int) {
+func requester(work chan<- lb.Request, nWorker int) {
 	c := make(chan int) // create a channel for receiving result for a particular requester
-	// each requester only allow to 10 requests
+	// each requester only allow to 10 requests to save time
 	for i := 0; i < 10; i++ {
 		// Kill some time (fake load). Do not flat out.
-		time.Sleep(time.Duration(rand.Int63n(1e3 * int64(nWorker))))
-		work <- loadbalancer.Request{Fn: workFn, Result: c} // send request, blocks
-		<-c                                                 // the result of workFn only returns boring 1, so discard by just draining the channel
+		// time.Sleep(time.Duration(rand.Int63n(1e3 / int64(nWorker))))
+		work <- lb.Request{Fn: workFn, Result: c} // send request, blocks
+		<-c                                       // the result of workFn only returns boring 1, so discard by just draining the channel
+		// below steps can be potentially added
 		// result := <-c              // wait for answer until there is one
 		// fmt.Println("Request has been processed, will send to furtherProcess()")
 		// furtherProcess(result)
@@ -31,51 +31,29 @@ func requester(work chan<- loadbalancer.Request, nWorker int) {
 }
 
 func main() {
-	// // Just a demonstration if there is no Worker and Balancer, how a Request
-	// // which was generated from a load generator is processed.
-	// r := make(chan Request)
-	// go requester(r)
-	// req := <-r
-	// req.c <- req.fn()
-	// // there is no wait for furtherProcess, so sleep a bit to let furtherProcess to finish
-	// time.Sleep(1 * time.Second)
-	// End of the simple dome
-
-	nRequester := 8
+	nRequester := 5 // this is the maximal pending total: each requester will wait until last request has completed before a new request is sent
 	nWorker := 3
-	wp := make(loadbalancer.Pool, nWorker)
+	wp := make(lb.Pool, nWorker)
 
 	for i := 0; i < nWorker; i++ {
-		w := loadbalancer.NewWorker(make(chan loadbalancer.Request, nRequester))
+		w := lb.NewWorker(make(chan lb.Request, 1))
 		wp[i] = &w
 	}
 
-	comp := make(chan *loadbalancer.Worker, nWorker)
-
-	// set all workers to share the same completion notification channel
+	comp := make(chan *lb.Worker, nWorker)
+	// set all workers with the same completion notification channel ready for receiving Requests
 	for _, w := range wp {
 		go w.Work(comp)
 	}
 
-	// Make a request channel for requester to send requests
-	r := make(chan loadbalancer.Request)
+	// Make a request channel for requester to send requests and run a few goroutines to generate requests
+	r := make(chan lb.Request)
 	for i := 0; i < nRequester; i++ {
 		go requester(r, nWorker)
 	}
 
-	// Set the Balancer up
-	b := loadbalancer.Balancer{}
+	// Set the Balancer up by passing on request and notification channels
+	b := lb.Balancer{}
+	// Balance has a timeout of 10s clause to exit
 	b.Balance(wp, r, comp)
-
-	// // Below creates requests non-stop, very quick resource runs out
-	// reaching to LLVM limit of 8128 live goroutines:
-	// race: limit on 8128 simultaneously alive goroutines is exceeded, dying
-	// go func() {
-	// 	for {
-	// 		go requester(r)
-	// 	}
-	// }()
-	// boom := time.After(10 * time.Second)
-	// <-boom
-	// fmt.Println("Too much, going home. BOOM!")
 }
