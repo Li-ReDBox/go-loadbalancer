06942e45170affb6aceb175ad542a4c0150296ee
Jianfeng Li <jianfeng.li@pwc.com>
2023-08-16 11:44:01 +0930

    A basic lb with buffered channels

--- cmd/buffered/main.go
+++ cmd/buffered/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 
 	lb "funmech.com/loadbalancer"
@@ -16,17 +17,10 @@ func workFn() int {
 // work is a send-only channel, once set, Balancer can start to dispatch
 func requester(work chan<- lb.Request, nWorker int) {
 	c := make(chan int) // create a channel for receiving result for a particular requester
-	// each requester only allow to 10 requests to save time
+	// in this design, the number of requests a requester run is not critical.
 	for i := 0; i < 3; i++ {
-		// Kill some time (fake load). Do not flat out.
-		// time.Sleep(time.Duration(rand.Int63n(1e3 / int64(nWorker))))
 		work <- lb.Request{Fn: workFn, Result: c} // send request, blocks
 		<-c                                       // the result of workFn only returns boring 1, so discard by just draining the channel
-		// below steps can be potentially added
-		// result := <-c              // wait for answer until there is one
-		// fmt.Println("Request has been processed, will send to furtherProcess()")
-		// furtherProcess(result)
-		// fmt.Println("furtherProcess has finished too. Full life cycle of a request is done.")
 	}
 }
 
@@ -35,28 +29,35 @@ func main() {
 	nWorker := 3
 	wp := make(lb.Pool, nWorker)
 
-	// when a non-buffered channel is used here, workers can hold up to 4 requests, then goes into deadlock
+	// Request channel of each Worker is set to the number of requesters
 	for i := 0; i < nWorker; i++ {
-		w := lb.NewWorker(make(chan lb.Request))
+		w := lb.NewWorker(make(chan lb.Request, nRequester))
 		wp[i] = &w
 	}
 
-	comp := make(chan *lb.Worker, 1)
-	// set all workers with the same completion notification channel ready for receiving Requests
+	comp := make(chan *lb.Worker, nWorker)
+	// set all workers with the same completion notification channel
 	for _, w := range wp {
 		go w.Work(comp)
 	}
 
-	// Make a request channel for requester to send requests and run a few goroutines to generate requests
+	// Make a request channel for requester to send requests
 	r := make(chan lb.Request)
 
 	// Set the Balancer up by passing on request and notification channels
 	b := lb.Balancer{}
-	// Balance has a timeout of 10s clause to exit
+	// Balance has a timeout of 10s clause to exit when its dispatch is not in deadlock!
 	go b.Balance(wp, r, comp)
 
+	// run a few goroutines to generate requests
+	var wg sync.WaitGroup
 	for i := 0; i < nRequester; i++ {
-		requester(r, nWorker)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			requester(r, nWorker)
+		}()
 	}
-
+	// Wait for all requests have been completed
+	wg.Wait()
 }
