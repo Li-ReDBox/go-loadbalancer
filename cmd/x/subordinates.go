19c78b73021943d44c272d50ac40962afc268ab3
Jianfeng Li <jianfeng.li@pwc.com>
2023-08-25 15:23:41 +0930

    Seems subordinates can be stopped

--- cmd/x/subordinates.go
+++ cmd/x/subordinates.go
@@ -12,13 +12,14 @@ import (
 // all subordinates stops doing what they were doing. Once every subordinate has reported they
 // have done, the controller exit.
 // subordinate works on two channels, if one is blocked, the other is blocked too, itself is blocked.
-func subordinate(no int, chat chan string, signal chan bool) {
+func subordinate(no int, chat chan string, signal chan string) {
 	s := 0
 	for {
 		s++
 		select {
 		case <-signal:
 			fmt.Println("subordinate", no+1, "has received signal")
+			signal <- fmt.Sprintf("ACK: subordinate %d has received signal", no+1)
 			return
 		default:
 			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
@@ -32,28 +33,31 @@ func subordinate(no int, chat chan string, signal chan bool) {
 func controller() {
 	nSub := 5
 	chat := make(chan string, nSub)
-	stop := make(chan bool) // not buffered, so stop channel is running in synchronous, easy to spot a deadlock
+	stop := make(chan string) // not buffered, so stop channel is running in synchronous, easy to spot a deadlock
+	stopped := make(chan struct{})
+
 	for i := 0; i < nSub; i++ {
 		go subordinate(i, chat, stop)
 	}
 
-	// The whole chatting can only last for 5 seconds
-	timeout := time.After(5 * time.Second)
+	go func() {
+		// The whole chatting can only last for 5 seconds
+		<-time.After(5 * time.Second)
+		for i := 0; i < nSub; i++ {
+			stop <- "please quit"
+			// now we can check if there is a response from a subordinate, but stop channel is synchronous channel, there is more a blocker
+			fmt.Println(<-stop)
+		}
+		stopped <- struct{}{}
+	}()
+
 	fmt.Println("Lets hear them")
 	for {
 		select {
 		case saying := <-chat:
 			fmt.Println(saying)
-		case <-timeout:
-			fmt.Println("timed out")
-			for i := 0; i < nSub; i++ {
-				stop <- true
-			}
-			// // return
-			// // default:
-			// fmt.Println("Sent 5 signals, sleep")
-			// time.Sleep(2 * time.Second)
-			// fmt.Println("Done in controller")
+		case <-stopped:
+			fmt.Println("All have been stopped, time to go home")
 			return
 		}
 	}
