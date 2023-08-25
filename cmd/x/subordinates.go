3368e97f929963b7b3d6078cf3bd713f9c80e195
Jianfeng Li <jianfeng.li@pwc.com>
2023-08-25 15:41:05 +0930

    When there are too many subordinates, there is still deadlock

--- cmd/x/subordinates.go
+++ cmd/x/subordinates.go
@@ -9,29 +9,30 @@ import (
 )
 
 // there a group of subordinates which listen to a channel, once a value is set in the channel,
-// all subordinates stops doing what they were doing. Once every subordinate has reported they
+// all subordinates will stops doing what they were doing but what have already started will not be
+// stopped until they are successfully completed. Once every subordinate has reported they
 // have done, the controller exit.
 // subordinate works on two channels, if one is blocked, the other is blocked too, itself is blocked.
+// signal channel is used by subordinate and controller to tell each other in a synchronous way.
 func subordinate(no int, chat chan string, signal chan string) {
 	s := 0
 	for {
 		s++
 		select {
 		case <-signal:
-			fmt.Println("subordinate", no+1, "has received signal")
-			signal <- fmt.Sprintf("ACK: subordinate %d has received signal", no+1)
+			fmt.Println("  subordinate", no+1, "has received signal")
+			signal <- fmt.Sprintf("    ACK: subordinate %d has received signal", no+1)
 			return
 		default:
-			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+			time.Sleep(time.Duration(rand.Intn(9000)) * time.Microsecond)
 			fmt.Printf("Subordinate %d's count is %d\n", no+1, s)
-			// cannot send to a closed channel, what is the solution?
 			chat <- fmt.Sprintf("%d's count is %d", no+1, s)
 		}
 	}
 }
 
 func controller() {
-	nSub := 5
+	nSub := 50
 	chat := make(chan string, nSub)
 	stop := make(chan string) // not buffered, so stop channel is running in synchronous, easy to spot a deadlock
 	stopped := make(chan struct{})
@@ -41,7 +42,8 @@ func controller() {
 	}
 
 	go func() {
-		// The whole chatting can only last for 5 seconds
+		// The whole chatting can only last for 5 seconds, but we are very kind to not shut anyone rashly, so, the slowest sayer will
+		// take some to quit, controller wait patiently
 		<-time.After(5 * time.Second)
 		for i := 0; i < nSub; i++ {
 			stop <- "please quit"
@@ -66,12 +68,12 @@ func controller() {
 func main() {
 	t := time.Now()
 	controller()
+	// The whole thing will not stop immediately, so it will be longer than 5s. If everyone only gives short speech, it could be quicker:
+	// say instead of  time.Sleep(time.Duration(rand.Intn(9000)) * time.Millisecond), time.Sleep(time.Duration(rand.Intn(9000)) * time.Microsecond)
+	// it will be very close to 5s.
 	fmt.Println("Chatted:", time.Since(t))
 
 	// if all reported, let's celebrate
-	// if main does not wait, channels are buffered, goroutines are left behind
-	// Even with waiting time, there still cases some subordinates are blocked at sending to chat channel. Why? chat channel is buffered, it should not happened!!!
-	// time.Sleep(5 * time.Second)
 	fmt.Println("Every subordinate has reported, we are done")
 	pprof.Lookup("goroutine").WriteTo(os.Stdout, 2)
 }
