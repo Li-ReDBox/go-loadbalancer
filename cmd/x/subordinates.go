f6250198c9f99ab94ef9386cf182454c3e734797
Jianfeng Li <jianfeng.li@pwc.com>
2023-08-28 12:11:51 +0930

    Add comments to subordinate code

--- cmd/x/subordinates.go
+++ cmd/x/subordinates.go
@@ -7,12 +7,12 @@ import (
 	"time"
 )
 
-// there a group of subordinates which listen to a channel, once a value is set in the channel,
+// there is a group of subordinates which listen to a channel, once a value is set in the channel,
 // all subordinates will stops doing what they were doing but what have already started will not be
 // stopped until they are successfully completed. Once every subordinate has reported they
 // have done, the controller exit.
-// subordinate works on two channels, if one is blocked, the other is blocked too, itself is blocked.
-// signal channel is used by subordinate and controller to tell each other in a synchronous way.
+// subordinate communicates with the controller through two channels: one for hearing and one
+// for acknowledging.
 func subordinate(no int, chat chan string, signal <-chan string, ack chan<- string) {
 	s := 0
 	no++
@@ -35,10 +35,10 @@ func subordinate(no int, chat chan string, signal <-chan string, ack chan<- stri
 }
 
 func controller() {
-	nSub := 150
+	nSub := 500
 	chat := make(chan string, nSub)
-	stop := make(chan string) // not buffered, so stop channel is running in synchronous, easy to spot a deadlock
-	ack := make(chan string)  // not buffered, so stop channel is running in synchronous, easy to spot a deadlock
+	stop := make(chan string) // not buffered, running in synchronous, easy to spot a deadlock
+	ack := make(chan string)  // not buffered, running in synchronous, easy to spot a deadlock
 	stopped := make(chan struct{})
 
 	for i := 0; i < nSub; i++ {
@@ -47,18 +47,18 @@ func controller() {
 
 	go func() {
 		// The whole chatting can only last for 5 seconds, but we are very kind to not shut anyone rashly, so, the slowest sayer will
-		// take some to quit, controller wait patiently
+		// take some to quit, controller wait patiently for everyone
 		<-time.After(5 * time.Second)
 		for i := 0; i < nSub; i++ {
+			// commanding for quitting
 			stop <- fmt.Sprintf("commanding %d: please quit", i+1)
-			// now we can check if there is a response from a subordinate, but stop channel is synchronous channel, there is more a blocker
+			// now we wait for acknowledgement. This is a blocking action
 			fmt.Printf("Stopped %d with its message: %s\n", i+1, <-ack)
 		}
 		stopped <- struct{}{}
 	}()
 
 	fmt.Println("Lets hear them")
-	// Now saying and stopped fight to each other, but why?
 	for {
 		select {
 		case saying := <-chat:
@@ -66,9 +66,6 @@ func controller() {
 		case <-stopped:
 			fmt.Println("All have been stopped, time to go home")
 			return
-			// default:
-			// 	// this does not really help, just avoid deadlock in some cases. What is a robust solution?
-			// 	time.Sleep(900)
 		}
 	}
 }
@@ -76,12 +73,13 @@ func controller() {
 func main() {
 	t := time.Now()
 	controller()
-	// The whole thing will not stop immediately, so it will be longer than 5s. If everyone only gives short speech, it could be quicker:
-	// say instead of  time.Sleep(time.Duration(rand.Intn(9000)) * time.Millisecond), time.Sleep(time.Duration(rand.Intn(9000)) * time.Microsecond)
-	// it will be very close to 5s.
+	// After speech, that subordinate goroutine has nothing blocks it and it goes to check stop signal immediately,
+	// if there is no signal, do speech again (sleep), which is a slow action compares to stop action. This goroutine
+	// is blocked. This guarantee speech is completed.
+	// The whole thing will not stop immediately, so it will be slightly longer than the allowed time.
 	fmt.Println("Chatted:", time.Since(t))
 
-	// if all reported, let's celebrate
+	// if all acknowledged, let's exit too
 	fmt.Println("Every subordinate has reported, we are done")
 	// pprof.Lookup("goroutine").WriteTo(os.Stdout, 2)
 }
