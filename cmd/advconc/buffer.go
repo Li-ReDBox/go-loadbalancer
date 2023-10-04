40ab890e6fd4f9813835d8b463ca592dd4df9c7f
Jianfeng Li <jianfeng.li@pwc.com>
2023-10-04 11:13:16 +1030

    Add comments to buffer.go

--- cmd/advconc/buffer.go
+++ cmd/advconc/buffer.go
@@ -1,12 +1,16 @@
 //go:build ignore && OMIT
 // +build ignore,OMIT
 
+// Modified the code example from https://go.dev/blog/io2013-talk-concurrency
+
 package main
 
 import (
 	"fmt"
 )
 
+// main runs synchronously:
+// batch send define number of data, close sending channel, drain receiving channel, then main goroutine exits.
 func main() {
 	in, out := make(chan int), make(chan int)
 	go buffer(in, out)
@@ -22,17 +26,25 @@ func main() {
 }
 
 // buffer provides an unbounded buffer between in and out.  buffer
-// exits when in is closed and all items in the buffer have been sent
+// exits when the in channel is closed and all items in the buffer have been sent
 // to out, at which point it closes out.
+// The buffer acts as a normal channel. Because it is unbounded, so it never blocks like which happens when a buffered channel reaches its size limit.
+// That's been said, it is not possible to define an unbounded buffered channel out of box.
+// Both receiving and sending channels it manages use close to signal completion.
+// Because it has unbounded buffer, it could be dangerous if buf grows quicker than shrinks as with any unbound data structures.
+// It can be blocked when both in and out channels are blocked.
 func buffer(in <-chan int, out chan<- int) {
 	var buf []int
+	// if in channel is not disabled (it will be disabled internally when it is closed outside), or there are data to be processed, run the loop.
 	for in != nil || len(buf) > 0 {
 		var i int
 		var c chan<- int
+		// if there are data in the queue, connect a local channel to the receiver's of this buffer.
 		if len(buf) > 0 {
 			i = buf[0]
 			c = out // enable send case
 		}
+		// read from in channel or send to out channel when anyone is ready
 		select {
 		case n, ok := <-in:
 			if ok {
@@ -40,7 +52,7 @@ func buffer(in <-chan int, out chan<- int) {
 			} else {
 				in = nil // disable receive case
 			}
-		case c <- i:
+		case c <- i: // send to receiver, and pop one out of top
 			buf = buf[1:]
 		}
 	}
